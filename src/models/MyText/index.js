027811eae13e628fba46182638fffdeec4e71fae
Mengqiking <mengqi.king@gmail.com>
2023-03-09 20:31:55 +0800

    upload

--- src/models/MyText/index.js
+++ src/models/MyText/index.js
@@ -1,43 +1,274 @@
-const data ={
-  lang:[
+const data = {
+  lang: [
     {
       value: 0,
-      label:'中文',
-      name:'zn_CN'
+      label: '中文',
+      name: 'zn_CN'
     }
   ]
   ,
-  Lang:[
-{
-value:0,
-label:'英语',
-name:'en_XX'
-},
-{
-value:1,
-label:'韩语',
-name:'ko_KR'
-},
-{
-value:2,
-label:'中文',
-name:'zh_CN'
-},
-{
-value:3,
-label:'西班牙',
-name:'es_XX'
-},
-{
-value:4,
-label:'俄语',
-name:'ru_RU'
-},
-{
-value:5,
-label:'日本',
-name:'ja_XX'
+  Lang:
+    [
+      {
+        value: 1,
+        label: '阿拉伯语',
+        name: 'ar_AR'
+      },
+      {
+        value: 2,
+        label: '捷克',
+        name: 'cs_CZ'
+      },
+      {
+        value: 3,
+        label: '德国',
+        name: 'de_DE'
+      },
+      {
+        value: 4,
+        label: '英语',
+        name: 'en_XX'
+      },
+      {
+        value: 5,
+        label: '西班牙',
+        name: 'es_XX'
+      },
+      {
+        value: 6,
+        label: '爱沙尼亚语',
+        name: 'et_EE'
+      },
+      {
+        value: 7,
+        label: '芬兰语',
+        name: 'fi_FI'
+      },
+      {
+        value: 8,
+        label: '法语',
+        name: 'fr_XX'
+      },
+      {
+        value: 9,
+        label: '古吉拉特语',
+        name: 'gu_IN'
+      },
+      {
+        value: 10,
+        label: '印地语',
+        name: 'hi_IN'
+      },
+      {
+        value: 11,
+        label: '意大利',
+        name: 'it_IT'
+      },
+      {
+        value: 12,
+        label: '日语',
+        name: 'ja_XX'
+      },
+      {
+        value: 13,
+        label: '哈萨克语',
+        name: 'kk_KZ'
+      },
+      {
+        value: 14,
+        label: '韩国',
+        name: 'ko_KR'
+      },
+      {
+        value: 15,
+        label: '立陶宛语',
+        name: 'lt_LT'
+      },
+      {
+        value: 16,
+        label: '拉脱维亚语',
+        name: 'lv_LV'
+      },
+      {
+        value: 17,
+        label: '缅甸语',
+        name: 'my_MM'
+      },
+      {
+        value: 18,
+        label: '尼泊尔语',
+        name: 'ne_NP'
+      },
+      {
+        value: 19,
+        label: '荷兰',
+        name: 'nl_XX'
+      },
+      {
+        value: 20,
+        label: '罗马尼亚语',
+        name: 'ro_RO'
+      },
+      {
+        value: 21,
+        label: '俄语',
+        name: 'ru_RU'
+      },
+      {
+        value: 22,
+        label: '僧伽罗语',
+        name: 'si_LK'
+      },
+      {
+        value: 23,
+        label: '土耳其',
+        name: 'tr_TR'
+      },
+      {
+        value: 24,
+        label: '越南语',
+        name: 'vi_VN'
+      },
+      {
+        value: 25,
+        label: '中文',
+        name: 'zh_CN'
+      },
+      {
+        value: 26,
+        label: '南非荷兰语',
+        name: 'af_ZA'
+      },
+      {
+        value: 27,
+        label: '阿塞拜疆语',
+        name: 'az_AZ'
+      },
+      {
+        value: 28,
+        label: '孟加拉语',
+        name: 'bn_IN'
+      },
+      {
+        value: 29,
+        label: '波斯',
+        name: 'fa_IR'
+      },
+      {
+        value: 30,
+        label: '希伯来语',
+        name: 'he_IL'
+      },
+      {
+        value: 31,
+        label: '克罗地亚语',
+        name: 'hr_HR'
+      },
+      {
+        value: 32,
+        label: '印度尼西亚语',
+        name: 'id_ID'
+      },
+      {
+        value: 33,
+        label: '格鲁吉亚语',
+        name: 'ka_GE'
+      },
+      {
+        value: 34,
+        label: '高棉语',
+        name: 'km_KH'
+      },
+      {
+        value: 35,
+        label: '马其顿语',
+        name: 'mk_MK'
+      },
+      {
+        value: 36,
+        label: '马拉雅拉姆语',
+        name: 'ml_IN'
+      },
+      {
+        value: 37,
+        label: '蒙古语',
+        name: 'mn_MN'
+      },
+      {
+        value: 38,
+        label: '马拉地语',
+        name: 'mr_IN'
+      },
+      {
+        value: 39,
+        label: '波兰语',
+        name: 'pl_PL'
+      },
+      {
+        value: 40,
+        label: '普什图语',
+        name: 'ps_AF'
+      },
+      {
+        value: 41,
+        label: '葡萄牙语',
+        name: 'pt_XX'
+      },
+      {
+        value: 42,
+        label: '瑞典语',
+        name: 'sv_SE'
+      },
+      {
+        value: 43,
+        label: '斯瓦希里语',
+        name: 'sw_KE'
+      },
+      {
+        value: 44,
+        label: '泰米尔语',
+        name: 'ta_IN'
+      },
+      {
+        value: 45,
+        label: '泰卢固语',
+        name: 'te_IN'
+      },
+      {
+        value: 46,
+        label: '泰语',
+        name: 'th_TH'
+      },
+      {
+        value: 47,
+        label: '塔加洛语',
+        name: 'tl_XX'
+      },
+      {
+        value: 48,
+        label: '乌克兰语',
+        name: 'uk_UA'
+      },
+      {
+        value: 49,
+        label: '乌尔都语',
+        name: 'ur_PK'
+      },
+      {
+        value: 50,
+        label: '科萨语',
+        name: 'xh_ZA'
+      },
+      {
+        value: 51,
+        label: '加利西亚语',
+        name: 'gl_ES'
+      },
+      {
+        value: 52,
+        label: '斯洛文尼亚语',
+        name: 'sl_S'
+      }
+    ]
 }
-]
-}
-export default {data}
+export default { data }
